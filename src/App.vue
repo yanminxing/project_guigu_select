ee7e775dca60e33726558bfbd768cf72076b3860
yanminxing <1194474469@qq.com>
2024-11-16 22:59:28 +0800

    feat(husky)：配置husky

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,19 @@
   <div class="box">
     <h1>Hello World</h1>
   </div>
-
-
-
-
 </template>
 
 <script setup lang="ts">
-var box = "111111111";
-
-
-
-
-
-
+var box = '111111111';
 </script>
 
 <style scoped>
 .box {
-  display:    flex;
-  justify-content:    center;
-  align-items:    center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   h1 {
-    height:     100px;
+    height: 100px;
   }
 }
 </style>
